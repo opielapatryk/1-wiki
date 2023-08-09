ef535c0d54900c809d9276bafb764965cfa752f5
opielapatryk <xpatrykopiela@gmail.com>
2023-08-09 14:06:45 +0200

    search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>",views.title, name="title"),
+    path("search",views.query, name="query"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django import forms
+from django.db.models import Q 
 
 from . import util
 
@@ -11,4 +13,29 @@ def title(request,title):
     return render(request, "encyclopedia/title.html", {
         "entries": util.get_entry(title),
         "title": title
+    })
+
+def is_subst_in_list(list,substring):
+    new_list = []
+    for string in list:
+        if substring in string:
+            new_list.append(string)
+    return new_list
+
+
+def query(request):
+    entries_list = []
+    new_list_entries = [x.lower() for x in util.list_entries()]
+    query = request.GET.get("q").lower()
+
+    entries_list = is_subst_in_list(new_list_entries,query)
+
+    if len(entries_list) > 0:
+        error_message = ""
+    else:
+        error_message = "That query didn't match any entries."
+
+    return render(request, "encyclopedia/query.html", {
+        "entries": entries_list,
+        "error_message": error_message
     })
