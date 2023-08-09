6ec9a083f221c5f3eab6e4cd65a36a8fe9389e5c
opielapatryk <xpatrykopiela@gmail.com>
2023-08-09 19:10:26 +0200

    random page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>",views.title, name="title"),
+    path("wiki/<str:title>",views.entry, name="entry"),
     path("search",views.query, name="query"),
     path("create",views.create, name="create"),
+    path("edit/<str:title>",views.edit, name="edit"),
+    path("random",views.rand, name="random"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,18 +1,42 @@
 from django.shortcuts import render
 from django import forms
 from . import util
+import random
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def title(request,title):
-    return render(request, "encyclopedia/title.html", {
-        "entries": util.get_entry(title),
+
+class EditContentForm(forms.Form):
+    content = forms.CharField(widget=forms.Textarea())
+
+def entry(request,title):
+    if request.method == "POST":
+        form = EditContentForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data["content"]
+            util.save_entry(title,entry)
+            return render(request, "encyclopedia/entry.html", {
+                    "entries": util.get_entry(title),
+                    "title": title,
+                })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entries": util.get_entry(title),
+            "title": title
+        })
+
+def edit(request,title):
+    initial = {'content': util.get_entry(title)}
+    return render(request, "encyclopedia/edit.html",{
+        "form":EditContentForm(initial=initial),
         "title": title
     })
 
+    
+    
 def is_subst_in_list(list,substring):
     new_list = []
     for string in list:
@@ -54,13 +78,13 @@ def create(request):
 
             if title not in new_list_entries:
                 util.save_entry(title,entry)
-                return render(request, "encyclopedia/title.html", {
+                return render(request, "encyclopedia/entry.html", {
                     "entries": util.get_entry(title),
                     "title": title,
                 })
             else:
                 error = 'This entry already exist.'
-                return render(request, "encyclopedia/title.html", {
+                return render(request, "encyclopedia/entry.html", {
                     "entries": error,
                     "title": title,
                 })
@@ -69,3 +93,9 @@ def create(request):
         "form":NewEntriesForm(),
         "title": title
     })
+
+def rand(request):
+    entries = util.list_entries()
+    rand_entry = random.choice(entries)
+    return render(request, "encyclopedia/entry.html", {"title":rand_entry,
+    "entries":util.get_entry(rand_entry)})
